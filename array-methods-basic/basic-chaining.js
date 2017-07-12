6b327dca7450c7da4c065e7a81f95edd4687ef76
MrDwina <diwina1993@hotmail.com>
2017-07-12 15:52:27 +0100

    I used .map & .filter instate of for loop

--- array-methods-basic/basic-chaining.js
+++ array-methods-basic/basic-chaining.js
@@ -1,16 +1,34 @@
 /*
     Refactor this to use .map and .filter together (chaining)
 */
-var vowels = ["a", "e", "i", "o", "u"];
+const vowels = ["a", "e", "i", "o", "u"];
 
+const getUpperCaseVowels = (letters)=>{
+    let result = [];
+    letters.map((params)=>{
+        (vowels.indexOf(params) !== -1) ? result.push(params.toUpperCase()) : false;
+    });
+    return result;
+};
+/*
+const getUpperCaseVowels = (letters)=>{
+    let result = [];
+    letters.filter(function (params) {
+        if (vowels.indexOf(params) !== -1) {
+            result.push(params.toUpperCase());
+        }
+    });
+    return result;
+};*/
+/*
 function getUpperCaseVowels(letters) {
-    var result = [];
-    for(var i = 0; i < letters.length; i++) {
+    const result = [];
+    for(const i = 0; i < letters.length; i++) {
         if(vowels.indexOf(letters[i]) !== -1) {
             result.push(letters[i].toUpperCase());
         }
     }
     return result;
-};
+};*/
 
 module.exports = getUpperCaseVowels;
