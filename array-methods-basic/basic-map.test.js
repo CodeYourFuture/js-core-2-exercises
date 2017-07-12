234c5fc79d34c2a65e03cfbb2c9c2921b502f1df
MrDwina <diwina1993@hotmail.com>
2017-07-12 15:58:33 +0100

    I added different test to check if it work propably

--- array-methods-basic/basic-map.test.js
+++ array-methods-basic/basic-map.test.js
@@ -7,3 +7,15 @@ describe('doubleNumber', function() {
         expect(result).toEqual([2, 6, 8]);
     });
 })
+
+test('doubles every number in the array', function () {
+    var numbers = [6, 8, 9];
+    var result = doubleNumber(numbers);
+    expect(result).toEqual([12, 16, 18]);
+});
+
+test('doubles every number in the array', function () {
+    var numbers = [2, 5, 7];
+    var result = doubleNumber(numbers);
+    expect(result).toEqual([4, 10, 14]);
+});
