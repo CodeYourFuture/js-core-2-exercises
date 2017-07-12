f641b0d5e935d3685e1e43913b387742aed07ba9
MrDwina <diwina1993@hotmail.com>
2017-07-12 15:57:15 +0100

    I used .map instate of for loop and I used ES6 Syntax

--- array-methods-basic/basic-map.js
+++ array-methods-basic/basic-map.js
@@ -3,12 +3,18 @@
 2. Write more tests to check it is working properly
 */
 
+const doubleAll = (numbers)=>{
+  let result = []
+  numbers.map((params => result.push(params * 2)));
+  return result
+}
+/*
 function doubleAll(numbers) {
   var result = []
   for (var i = 0; i < numbers.length; i++) {
     result.push(numbers[i] * 2)
   }
   return result
-}
+}*/
 
 module.exports = doubleAll;
