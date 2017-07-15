994393790e3b157e6f3aeaf6ce3e502d640cd5ff
MrDwina <diwina1993@hotmail.com>
2017-07-15 22:06:48 +0100

    I created function to remove vowels and print others letters with change them to upper case

--- string-manipulation/remove-vowels-and-capitalise.js
+++ string-manipulation/remove-vowels-and-capitalise.js
@@ -4,12 +4,29 @@
     1. Remove skip from the test (see it fail in your console - RED)
     2. Implement the function so that the test passes (Green)
     3. Try and use all four of .split, .filter, .map, and .join in your implementation (if you don't any of these Google it)
+
 */
-
 var vowels = ["a", "e", "i", "o", "u"];
-
+/*
 function removeVowelsAndCapitalise(letters) {
     // your code here - use .split, .filter, .map and .join in your implementation
+    letters.split('').filter(function (item) {
+        return item;
+    })
 };
 
+module.exports = removeVowelsAndCapitalise;
+*/
+//var test = removeVowelsAndCapitalise('CodeYourFuture');
+function removeVowelsAndCapitalise(letters) {
+    const result = letters.split('').filter((letter) => {
+        return letter;
+    }).map((letter) => {
+         if(!vowels.includes(letter)) {
+             return letter.toUpperCase();
+        }      
+    }).join('');
+    return result;
+};
+//console.log(removeVowelsAndCapitalise(test));
 module.exports = removeVowelsAndCapitalise;
