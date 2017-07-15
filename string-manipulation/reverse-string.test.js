2312cde3c15963f3048020ea589024f640aa8f56
MrDwina <diwina1993@hotmail.com>
2017-07-15 22:13:11 +0100

    I removed skip and added new test

--- string-manipulation/reverse-string.test.js
+++ string-manipulation/reverse-string.test.js
@@ -1,10 +1,15 @@
 var reverseString = require('./reverse-string');
 
-describe.skip('reverseString', function() {
+describe('reverseString', function() {
     test('should reverse CodeYourFuture', function(){
         var originalString = 'CodeYourFuture';
         var result = reverseString(originalString);
         expect(result).toEqual('erutuFruoYedoC');
+    });
+    test('should reverse MohammedDwina', function () {
+        var originalString = 'MohammedDwina';
+        var result = reverseString(originalString);
+        expect(result).toEqual('aniwDdemmahoM');
     });
 
     //add more tests
