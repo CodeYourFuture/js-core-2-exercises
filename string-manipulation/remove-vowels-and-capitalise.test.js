3e8cdd72fff5bdc6282edc25e9a6111a8d01e0e8
MrDwina <diwina1993@hotmail.com>
2017-07-15 22:08:00 +0100

    I removed skip

--- string-manipulation/remove-vowels-and-capitalise.test.js
+++ string-manipulation/remove-vowels-and-capitalise.test.js
@@ -1,6 +1,6 @@
 var removeVowelsAndCapitalise = require('./remove-vowels-and-capitalise');
 
-describe.skip('removeVowelsAndCapitalise', function() {
+describe('removeVowelsAndCapitalise', function() {
     test('removes vowels and capitalises', function() {
         var result = removeVowelsAndCapitalise('CodeYourFuture');
         expect(result).toEqual('CDYRFTR');
