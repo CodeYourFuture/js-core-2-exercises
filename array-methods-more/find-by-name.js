4c5dfca0e8c3853c4e88c21292e059cec358656b
YJohn <yfekadu2016@gmail.com>
2017-07-20 18:17:19 +0100

    find the person with his first name using .find and ES6 syntax

--- array-methods-more/find-by-name.js
+++ array-methods-more/find-by-name.js
@@ -2,13 +2,22 @@
   Refactor this function to use .find and ES6 syntax
  */
 
+// function findByName(persons, name) {
+//   for(var i = 0; i < persons.length; i++) {
+//     if(persons[i].firstName === name) {
+//       // notice how this returns once it finds a match (it breaks the loop)
+//       return persons[i];
+//     }
+//   }
+// };
+
+// module.exports = findByName;
+
 function findByName(persons, name) {
-  for(var i = 0; i < persons.length; i++) {
-    if(persons[i].firstName === name) {
-      // notice how this returns once it finds a match (it breaks the loop)
-      return persons[i];
-    }
-  }
+  var result = persons.find(function(person){
+    return person.firstName === name
+  })
+  return result;
 };
 
 module.exports = findByName;
